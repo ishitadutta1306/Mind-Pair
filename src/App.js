5b5221c823a7d0ae09b15678b59232dad9117a24
Ishita Dutta <ishita130602@gmail.com>
2024-10-27 22:11:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';  //hooks
 import './App.css';
 import Card from './Components/Card';
 
@@ -13,10 +13,10 @@ const cardImages=[
 
 const App=()=>{
   //hold the shuffled deck of cards
-  const [cards,setCards]=useState([]);  //'cards' state: empty array with useState([])
+  const [cards,setCards]=useState([]);  //'cards' state: empty array with useState([]) & 'setCards' - a func to update the 'cards' state whenever the deck changes
 
   //hide moves info when the component first loads
-  const [showMoves, setShowMoves]=useState(false);
+  const [showMoves, setShowMoves]=useState(false);  //'showMoves' - a boolean that determines if move info should be shown, whose initial value is set to false , 'setShowMoves' - updates the 'showMoves' state to toggle visibility
 
   //keep track of the number of moves taken
   const [moves,setMoves]=useState(0);   //initializes the 'turns' state with a value of 0
@@ -31,32 +31,36 @@ const App=()=>{
   //shuffle cards
   const shuffleCards=()=>{
     const shuffleCards=[...cardImages,...cardImages] //...: copy or merge arrays-> duplicating card images to have pairs of each card
-      .sort(()=>Math.random()-0.5)  //generates a random floating-point number between 0 and 1, subtracting 0.5 shifts this range to -0.5 to +0.5, for [A, B]: A stays before B if result -ve
-      .map((card)=>({...card, id: Math.random()})); //maps over each card to add a unique id to every card object
-      setCards(shuffleCards);
+      .sort(()=>Math.random()-0.5)  //generates a random floating-point number between 0 and 1, subtracting 0.5 shifts this range to -0.5 to +0.5,where each comparison randomly results in positive or negative, creating a random card order, for [A, B]: A stays before B if result -ve
+      .map((card)=>({...card, id: Math.random()})); //iterates over each card and creates a new card object, itcopies all properties from the original card using ... and adds a unique id property with Math.random() for tracking each card individually
+
+      //update the states
+      setCards(shuffleCards); //updates the 'cards' state with the newly shuffled and duplicated cards array, containing each card in a randomized position with unique identifiers
       setShowMoves(false);
       setMoves(0);
+
+      //clear any previous selections, ensuring no card selections carry over from the previous game
       setChoice1(null);
       setChoice2(null);
   }
 
   console.log(cards,moves);
 
-  //handle a choice
-  const handleChoice=(card)=>{
+  //handles the player's selection of a card, updates the choices, and controls the display of move-related info 
+  const handleChoice=(card)=>{  //'card' is the parameter representing the card object that the player has selected
     setShowMoves(true);
     console.log(card);
-    choice1?setChoice2(card):setChoice1(card);  //update choiceTwo if 1st choice already made
+    choice1?setChoice2(card):setChoice1(card);  //update choice2 if 1st choice already made
   }
 
   //compare the 2 selected cards
-  useEffect(()=>{
+  useEffect(()=>{  //this hook runs whenever choice1 or choice2 changes
     if (choice1 && choice2){  //checks if we have a value for both choice1 & choice2
-      setDisabled(true);
+      setDisabled(true);  //temporarily disable the game board
       
       if (choice1.src===choice2.src){
-        setCards(prevCards=>{
-          return prevCards.map(card=>{
+        setCards(prevCards=>{ //new array created within the callback function: represents the current state of cards before the update, passed to the func
+          return prevCards.map(card=>{  //iterates over each 'card' object in 'prevCards' 
             if (card.src===choice1.src){
               return {...card,matched:true};
             }
